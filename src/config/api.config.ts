1ba0a7ff4bb1c2faabfc8bb9d196c5e7fe9a446a
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-09-11 11:20:59 -0300

    Testando no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
     //url base do web service
-    baseUrl : "http://localhost:8080",
+    baseUrl : "http://192.168.0.247:8080",
     bucketBaseUrl : "https://s3-sa-east-1.amazonaws.com/natanlf-curso-spring-ionic"
 }
